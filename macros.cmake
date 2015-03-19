eeb507ceac89ec16eeaf6b1dcdf18aa39fc47e9d
Timofey Turenko <timofey.turenko@skysql.com>
2015-03-19 10:14:06 +0200

    first commit

---
+++ macros.cmake
@@ -0,0 +1,20 @@
+macro(set_maxscale_version)
+
+  #MaxScale version number
+  set(MAXSCALE_VERSION_MAJOR "1")
+  set(MAXSCALE_VERSION_MINOR "1")
+  set(MAXSCALE_VERSION_PATCH "1")
+  set(MAXSCALE_VERSION_NUMERIC "${MAXSCALE_VERSION_MAJOR}.${MAXSCALE_VERSION_MINOR}.${MAXSCALE_VERSION_PATCH}")
+  set(MAXSCALE_VERSION "${MAXSCALE_VERSION_MAJOR}.${MAXSCALE_VERSION_MINOR}.${MAXSCALE_VERSION_PATCH}")
+
+endmacro()
+
+macro(set_variables)
+
+  # Installation directory
+  set(INSTALL_DIR "/usr/local/skysql/maxscale/" CACHE PATH "MaxScale installation directory.")
+  
+  # Build type
+  set(BUILD_TYPE "None" CACHE STRING "Build type, possible values are:None, Debug, Optimized.")
+  
+endmacro()
---
+++ setup_maxscale_repos.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+path_to_repo="http://jenkins.engskysql.com/repository/develop/mariadb-maxscale/1.0/"
+distro_name="centos"
+maxdir="/usr/local/skysql/maxscale/repo-setup"
+
+zypper --version 2> /dev/null > /dev/null
+if [[ $? == 0 ]]; then
+	# it is SUSE
+	release_info=$(cat /etc/*-release 2>/dev/null)
+	if [[ $(echo "$release_info" | grep 'VERSION = 13') != "" ]]; then
+		# we have opensuse only for version 13 (should be fixed later)
+                distro_name="opensuse"
+		sed "s/####path_to_repo####/$path_to_repo/g" $maxdir/maxscale.repo.suse13.template > /etc/zypp/repos.d/maxscale.repo
+	else
+                distro_name="sles"
+		if [[ $(echo "$release_info" | grep 'VERSION = 12') != "" ]]; then
+			sed "s/####path_to_repo####/$path_to_repo/g" $maxdir/maxscale.repo.sles12.template > /etc/zypp/repos.d/maxscale.repo
+		else
+			sed "s/####path_to_repo####/$path_to_repo/g" $maxdir/maxscale.repo.sles11.template > /etc/zypp/repos.d/maxscale.repo
+		fi
+        fi
+else
+	yum --version 2> /dev/null > /dev/null
+	if [[ $? == 0 ]]; then
+		# there is YUM here
+		release_info=$(cat /etc/*-release 2>/dev/null)
+		if [[ $(echo "$release_info" | grep 'Red Hat') != "" ]]; then
+			distro_name="rhel"
+		fi
+		if [[ $(echo "$release_info" | grep 'fedora') != "" ]]; then
+        		distro_name="fedora"
+		fi
+
+		sed "s/####path_to_repo####/$path_to_repo/g" $maxdir/maxscale.repo.template | sed "s/###distro_name###/$distro_name/g"> /etc/yum.repos.d/maxscale.repo
+	fi
+fi
+
+apt-get --version 2> /dev/null > /dev/null
+if [[ $? == 0 ]]; then
+	# there is apt-get here
+	release_info=$(cat /etc/*-release 2>/dev/null)
+	if [[ $(echo "$release_info" | grep 'Ubuntu') != "" ]]; then
+		distro_name="ubuntu"
+	else 
+        	distro_name="debian"
+	fi
+	distro_codename=$(cat /etc/*-release 2>/dev/null | grep "DISTRIB_CODENAME" | sed "s/DISTRIB_CODENAME=//")
+	sed "s/####path_to_repo####/$path_to_repo/g" $maxdir/maxscale.repo.template | sed "s/###distro_name###/$distro_name/g" |sed "s/###distro_codename###/$distro_codename/g" > /etc/apt/sources.list.d/maxscale.list
+fi
